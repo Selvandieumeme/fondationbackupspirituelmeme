416aa7eea5c8d2fa6c30e999a0260e1e03c973f2
Selvandieumeme <infos@fondationbackupspirituel.com>
2025-07-07 18:15:53 -0400

    Update get_ambassadors.php

--- get_ambassadors.php
+++ get_ambassadors.php
@@ -1,12 +1,21 @@
 <?php
-// get_ambassadors.php
-$html = file_get_contents("Nos-Ambassadeurs.html");
-
-// Regex pou rale non yo ki nan <li> oswa <div class="ambassadeur-name">, modifye dapre estrikti ou
-preg_match_all('/<h3[^>]*>(.*?)<\/h3>/i', $html, $matches);
-$ambassadeurs = array_unique(array_map('trim', $matches[1]));
-
 header('Content-Type: application/json');
-echo json_encode($ambassadeurs);
+
+// Chemen fichye anbasadè yo
+$ambFile = __DIR__ . '/Nos-ambassadeurs.html';
+$ambNames = [];
+
+if (file_exists($ambFile)) {
+    $html = file_get_contents($ambFile);
+
+    // Rechèch non yo nan <td> ki vini apre nimewo anbasadè a
+    if (preg_match_all('/<td>\s*\d+\s*<\/td>\s*<td>(.*?)<\/td>/i', $html, $matches)) {
+        $ambNames = array_unique(array_map('trim', $matches[1]));
+        sort($ambNames, SORT_LOCALE_STRING);
+    }
+}
+
+// Retounen lis anbasadè yo an JSON
+echo json_encode($ambNames, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
 exit;
 ?>
