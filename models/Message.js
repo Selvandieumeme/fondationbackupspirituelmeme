5a21844f17e4f82126d802bf14bd9f008cb970c1
Selvandieumeme <infos@fondationbackupspirituel.com>
2025-07-12 15:26:59 -0400

    Update Message.js

--- models/Message.js
+++ models/Message.js
@@ -1,27 +1,27 @@
 const mongoose = require('mongoose');
 
 const messageSchema = new mongoose.Schema({
-  eglise: { type: String, required: true },               // Nom de l'Église
-  zone: { type: String, required: true },                 // Nom de la zone ou ville
-  pasteur: { type: String, required: true },              // Nom du Pasteur Principal
-  comite1: String,                                        // Nom du premier membre du comité
-  comite2: String,                                        // Nom du deuxième membre du comité
-  adresse: { type: String, required: true },              // Adresse de l'église
-  tel: String,                                            // Téléphone
-  email: String,                                          // Email de l'église
-  whatsapp: String,                                       // Numéro WhatsApp du Pasteur
-  programme: String,                                      // Programmes de la semaine
-  activites: String,                                      // Autres activités
-  membres: Number,                                        // Nombre de membres
-  veye_nwi: String,                                       // Veillée de nuit ?
-  le_veye: String,                                       // Jour / Heure veillée
-  jeun: String,                                          // Jeûne de prière ?
-  le_jeun: String,                                       // Jour / Heure jeûne
-  facebook: String,                                      // Page Facebook
-  youtube: String,                                       // Chaîne YouTube
-  en_construction: String,                               // Église en construction ?
-  photos: [String],                                      // Noms fichiers photos
-  videos: [String],                                      // Noms fichiers vidéos
+  eglise: String,
+  zone: String,
+  pasteur: String,
+  comite1: String,
+  comite2: String,
+  adresse: String,
+  tel: String,
+  email: String,
+  whatsapp: String,
+  programme: String,
+  activites: String,
+  membres: Number,
+  veye_nwi: String,
+  le_veye: String,
+  jeun: String,
+  le_jeun: String,
+  facebook: String,
+  youtube: String,
+  en_construction: String,
+  photos: [String],  // lis non fichye
+  videos: [String],  // lis non fichye
   date: {
     type: Date,
     default: Date.now,
