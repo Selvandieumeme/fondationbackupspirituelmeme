73cc2a754f07e9a91f10447e8c2e940b7e036065
Selvandieumeme <infos@fondationbackupspirituel.com>
2025-07-11 21:35:09 -0400

    Update load.php

--- fobas-eglises-nord-ouest/load.php
+++ fobas-eglises-nord-ouest/load.php
@@ -1,1 +1,10 @@
+<?php
+header('Content-Type: application/json');
+$dataFile = 'eglises_data.json';
 
+if (file_exists($dataFile)) {
+    echo file_get_contents($dataFile);
+} else {
+    echo json_encode([]);
+}
+?>
