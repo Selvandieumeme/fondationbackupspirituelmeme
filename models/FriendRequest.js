53d1b7eba2165242fb1f6d5d9794a3c7db2fbb7d
Selvandieumeme <infos@fondationbackupspirituel.com>
2025-07-13 10:03:39 -0400

    Delete models/FriendRequest.js

--- models/FriendRequest.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-const frSchema = new mongoose.Schema({
-  from: { type: mongoose.Types.ObjectId, ref: 'User', required: true },
-  to:   { type: mongoose.Types.ObjectId, ref: 'User', required: true },
-  status: { type: String, enum: ['pending', 'accepted', 'rejected'], default: 'pending' }
-}, { timestamps: true });
-
-module.exports = mongoose.model('FriendRequest', frSchema);
