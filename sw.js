1a4ef09214d2a836f073d8ec13ba722e2349c5c0
Selvandieumeme <infos@fondationbackupspirituel.com>
2025-07-27 13:58:06 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -45,12 +45,3 @@ self.addEventListener('fetch', event => {
 });
 
 
-<script>
-  if ('serviceWorker' in navigator) {
-    window.addEventListener('load', () => {
-      navigator.serviceWorker.register('/sw.js')
-        .then(reg => console.log('✅ Service Worker registered!', reg))
-        .catch(err => console.error('❌ Service Worker registration failed:', err));
-    });
-  }
-</script>
