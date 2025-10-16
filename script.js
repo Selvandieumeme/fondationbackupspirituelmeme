19bcf035e3e07f062c44dbf9d338d06dcb305041
Selvandieumeme <infos@fondationbackupspirituel.com>
2025-10-16 14:18:56 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,32 @@
 // script.js
+let username = localStorage.getItem('username');
+
+// Si itilizatè a poko gen non, montre overlay la
+window.addEventListener('load', () => {
+  if (!username) {
+    document.getElementById('loginOverlay').style.display = 'flex';
+  } else {
+    document.getElementById('loginOverlay').style.display = 'none';
+  }
+});
+
+// Bouton pou antre nan chat la
+document.getElementById('enterChatBtn').addEventListener('click', () => {
+  const input = document.getElementById('usernameInput');
+  const name = input.value.trim();
+
+  if (name) {
+    username = name;
+    localStorage.setItem('username', name);
+    document.getElementById('loginOverlay').style.display = 'none';
+  } else {
+    alert('Tanpri antre yon non avan ou antre nan chat la.');
+  }
+});
+
+
+
+
 const socket = io();
 
 // Rekipere eleman yo
