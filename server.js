0fc09216f03bcdcb7343d874c0e27423d4c5a251
Selvandieumeme <infos@fondationbackupspirituel.com>
2025-10-20 01:07:53 -0400

    Update server.js

--- server.js
+++ server.js
@@ -262,12 +262,6 @@ app.get('/ping', (req, res) => res.send('pong'));
 // 💬 SOCKET.IO CHAT — VÈSYON FINAL AK LISTE ITILIZATÈ AKTIF
 // ---------------------------
 
-const mongoose = require('mongoose');
-const http = require('http');
-const express = require('express');
-const { Server } = require('socket.io');
-const app = express();
-
 // ---------------------------
 // 📦 Mongoose Schema
 // ---------------------------
