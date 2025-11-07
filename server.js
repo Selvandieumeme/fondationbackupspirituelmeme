4a17abadc970028da726f191a43cb59eafe14719
Selvandieumeme <infos@fondationbackupspirituel.com>
2025-11-07 11:59:32 -0500

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,6 @@
 // ============================
 require('dotenv').config(); // ✅ Kenbe sa nan tèt
 
-require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
