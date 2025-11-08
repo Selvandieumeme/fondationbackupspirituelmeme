6ecdcf68376d745d7ec82342431e072ff2cdc928
Selvandieumeme <infos@fondationbackupspirituel.com>
2025-11-08 13:03:47 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1097,12 +1097,7 @@ io.on('connection', socket => {
 
 
 
-// --- MongoDB connect ---
-const MONGO_URI = process.env.MONGO_URI;
-if (!MONGO_URI) {
-  console.error('MONGO_URI not set in environment. Exiting.');
-  process.exit(1);
-}
+
 
 mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('✅ MongoDB connected'))
