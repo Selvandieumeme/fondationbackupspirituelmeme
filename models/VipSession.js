9368dc45412b1684ac01a038b0e4c47b35d8f0ed
Selvandieumeme <infos@fondationbackupspirituel.com>
2025-11-13 00:08:30 -0500

    Create VipSession.js

---
+++ models/VipSession.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+const VipSessionSchema = new mongoose.Schema({
+  nom: { type: String, required: true },
+  dateNaissance: { type: Date, required: true },
+  ville: { type: String, required: true },
+  pays: { type: String, required: true },
+  whatsapp: { type: String, required: true },
+  email: { type: String, required: true },
+  passwordHash: { type: String, required: true },
+  emailRecup: { type: String, required: true },
+  methodePaiement: { type: String, required: true },
+  montant: { type: Number, required: true },
+  preuvePath: { type: String, required: true },
+  statut: { type: String, default: "pending" },
+  createdAt: { type: Date, default: Date.now }
+});
+
+export default mongoose.model("VipSession", VipSessionSchema, "sessions");
