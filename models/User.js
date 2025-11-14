02a9ab766637d344e9485e3c263b1cae48a6162a
Selvandieumeme <infos@fondationbackupspirituel.com>
2025-11-14 01:11:21 -0500

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema({
-  fullname: { type: String, required: true },
+  name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   passwordHash: { type: String, required: true },
   role: { type: String, default: "VIP" },
