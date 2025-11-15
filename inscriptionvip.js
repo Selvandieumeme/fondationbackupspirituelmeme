daed7e604d4a9e24df364bcbc4947d31fa0c35d9
Selvandieumeme <infos@fondationbackupspirituel.com>
2025-11-14 23:29:21 -0500

    Update inscriptionvip.js

--- inscriptionvip.js
+++ inscriptionvip.js
@@ -30,7 +30,7 @@ document.getElementById('vip-register-form').addEventListener('submit', async fu
 
     try {
         // Chanje URL sa pou li konekte ak backend lokal ou a
-        const res = await fetch('http://localhost:4000/api/sessions', {
+        const res = await fetch('https://unconscionably-hexameral-margot.ngrok-free.dev/api/sessions', {
             method: 'POST',
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify(dataToSend)
