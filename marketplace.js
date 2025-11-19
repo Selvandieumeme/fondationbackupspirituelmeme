2fc86e23111ca09ead83252e48d1d4723ac5435f
Selvandieumeme <infos@fondationbackupspirituel.com>
2025-11-19 17:43:25 -0500

    Update marketplace.js

--- marketplace.js
+++ marketplace.js
@@ -1,4 +1,4 @@
-const API_BASE = "https://fondationbackupspirituelmeme-vip.vercel.app";
+const res = await fetch(`${API_BASE}/api/products`);
 
 // -----------------------------
 // Sidebar & Ajout Store
