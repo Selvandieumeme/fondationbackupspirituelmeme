382cbba4fd8c2b8f1978ac346a986945f4bd012c
Selvandieumeme <infos@fondationbackupspirituel.com>
2026-01-17 22:03:17 -0500

    Update noswalletsfobasagents.js

--- noswalletsfobasagents.js
+++ noswalletsfobasagents.js
@@ -99,7 +99,7 @@ const agentsData = {
   { name:"DUPRE Hilsaint", zone:"Temps Perdu, Mare Rouge", services:"Manager Nord-Ouest", phone:"50932303047", photo:"Hilsaintdupre.jpg" },
   { name:"Petide Weetbency", zone:"Bombardopolis", services:"Agent Autorisé", phone:"50943294137", photo:"Petitdewensbenleyagentbonbad.jpg" },
   { name:"Mentor Kerlin", zone:"Clenette, Bombardopolis", services:"Agent Autorisé", phone:"50932299183", photo:"MentorKerlinbonbad.jpg" },
-  { name:"Elsa Michel", zone:"Port-de-Paix", services:"Agent Autorisé", phone:"50940707004", photo:"ElsaNO.jpg" },
+  { name:"Sanon Pierre-Andre", zone:"Rue Bouco, Clenette", services:"Representant Commune Bombardopolis", phone:"50943428235", photo:"Sanonpierreandre.jpg" },
   { name:"Patrick Jean", zone:"Saint-Louis-du-Nord", services:"Agent Autorisé", phone:"50940707005", photo:"PatrickNO.jpg" },
   { name:"Sonia Pierre", zone:"Jean-Rabel", services:"Agent Autorisé", phone:"50940707006", photo:"SoniaNO.jpg" },
   { name:"Junior Noel", zone:"Port-de-Paix", services:"Agent Autorisé", phone:"50940707007", photo:"JuniorNO.jpg" },
