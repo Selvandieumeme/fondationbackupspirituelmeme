e66bf2d670f5ed0d3f4cf281381e137a52c673a3
Selvandieumeme <infos@fondationbackupspirituel.com>
2026-02-01 15:34:52 -0500

    Add registerMerchant function for merchant creation
    Implement merchant registration with unique ID and API key generation.

--- merchant/merchant.controller.js
+++ merchant/merchant.controller.js
@@ -1,1 +1,29 @@
+const crypto = require('crypto');
+const Merchant = require('./merchant.model');
 
+exports.registerMerchant = async (req, res) => {
+  try {
+    const { businessName, ownerName, email, phone } = req.body;
+
+    const merchantId = 'MER-' + crypto.randomBytes(4).toString('hex');
+    const apiKey = crypto.randomBytes(32).toString('hex');
+
+    const merchant = await Merchant.create({
+      businessName,
+      ownerName,
+      email,
+      phone,
+      merchantId,
+      apiKey
+    });
+
+    res.json({
+      message: 'Commerçant créé',
+      merchantId,
+      apiKey
+    });
+
+  } catch (e) {
+    res.status(500).json({ message: 'Erreur création commerçant' });
+  }
+};
