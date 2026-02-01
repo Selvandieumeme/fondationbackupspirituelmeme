9030d1279518839a1123ba4117233ba36a26a1c9
Selvandieumeme <infos@fondationbackupspirituel.com>
2026-02-01 15:36:59 -0500

    Add merchant registration route

--- merchant/merchant.routes.js
+++ merchant/merchant.routes.js
@@ -1,1 +1,7 @@
+const express = require('express');
+const router = express.Router();
+const controller = require('./merchant.controller');
 
+router.post('/register', controller.registerMerchant);
+
+module.exports = router;
