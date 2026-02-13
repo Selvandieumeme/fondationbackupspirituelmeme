dba64b2a645fa025d0dec3cc8e5274186f38c153
Selvandieumeme <infos@fondationbackupspirituel.com>
2026-02-13 02:45:41 -0500

    Update server.js

--- server.js
+++ server.js
@@ -86,9 +86,7 @@ app.use('/wallet', walletToMerchantRoutes);   // <-- sa pèmèt POST /wallet/tra
 
 
 
-// Konekte express routes pou Transfert Express Haiti
-const expressRoutes = require('./express/express.routes');
-app.use('/api/express', expressRoutes);
+
 
 
 
