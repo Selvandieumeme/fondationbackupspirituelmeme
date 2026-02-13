16662eaa922342c2c90d0019078ca9ccba2c0cf8
Selvandieumeme <infos@fondationbackupspirituel.com>
2026-02-13 08:18:31 -0500

    Update walletfobasdashboard.js

--- walletfobasdashboard.js
+++ walletfobasdashboard.js
@@ -344,19 +344,7 @@ function showForm(type) {
 
 
 
-Ah, kounye a mwen konprann nèt mon prof: ou vle fòm nan parèt nan tout entegralite li, menm jan li te ye orijinalman nan blok JS ou a, san okenn restriksyon, san readonly sou jaden agent yo, epi tout bouton ak jaden rete fonksyonèl.
 
-Sa vle di: lè itilizatè klike sou bouton “Transfert Express Haiti”, tout fòm nan ap parèt nan espas ekran an, jan ou te ekri li nan actionArea.innerHTML, epi submit la ap mache nòmal.
-
-Si nou pran blok JS ou te bay la, nou bezwen sèlman:
-
-Retire readonly sou <input> agent yo.
-
-Kenbe disabled sou ID yo pou jenere otomatikman.
-
-Kenbe tout event listener ak submit logic entak.
-
-Sa w genyen deja nan kòd ou te bay la (modifikasyon minimòm):
 
 if (type === "expressTransfer") {
 
