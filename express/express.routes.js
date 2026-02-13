20d8bba8745081731c122277acf351cb5a62167a
Selvandieumeme <infos@fondationbackupspirituel.com>
2026-02-12 20:30:41 -0500

    Update express.routes.js

--- express/express.routes.js
+++ express/express.routes.js
@@ -1,1 +1,15 @@
+// /express/express.routes.js
 
+const express = require('express');
+const router = express.Router();
+
+const controller = require('./express.controller');
+const authMiddleware = require('./express.auth.js');
+
+// Base URL: https://api.fondationbackupspirituel.com/api/express
+
+router.post('/create', authMiddleware, controller.createTransfer);
+router.post('/verify-otp', authMiddleware, controller.verifyOTP);
+router.post('/pay', authMiddleware, controller.markAsPaid);
+
+module.exports = router;
