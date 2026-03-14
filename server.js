16d96e231bd652a4ddde22d2f2cedb878fc8b378
Selvandieumeme <infos@fondationbackupspirituel.com>
2026-03-14 14:41:44 -0400

    Add transfer routes to API

--- server.js
+++ server.js
@@ -86,7 +86,8 @@ app.use('/wallet', walletToMerchantRoutes);   // <-- sa pèmèt POST /wallet/tra
 
 
 
-
+const transferRoutes = require("./fobas-transfer-module/transfer.routes");
+app.use("/api", transferRoutes);
 
 
 
