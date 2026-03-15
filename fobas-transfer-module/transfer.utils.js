9bddf83195b92ee344a720746ab510d6e3334c27
Selvandieumeme <infos@fondationbackupspirituel.com>
2026-03-14 20:58:03 -0400

    Refactor code for unique code generation
    Refactored code to create a unique code generator and removed the previous code generation and fee calculation functions.

--- fobas-transfer-module/transfer.utils.js
+++ fobas-transfer-module/transfer.utils.js
@@ -1,18 +1,13 @@
-// ==================== FONKSYON UTILITAIRES FOBAS ====================
+function generateCodeUnique(length = 10) {
 
-// Jenere yon kòd inik pou transfè a
-function generateCode() {
-  const a = Math.floor(1000 + Math.random() * 9000);
-  const b = Math.floor(1000 + Math.random() * 9000);
-  return `FBS-${a}-${b}`;
+  const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+  let code = "";
+
+  for (let i = 0; i < length; i++) {
+    code += chars.charAt(Math.floor(Math.random() * chars.length));
+  }
+
+  return code;
 }
 
-// Kalkile frais transfè a (HTG sèlman)
-function calculateFee(montant) {
-  return Math.round(montant * 0.15); // 15% frais sou montant
-}
-
-module.exports = {
-  generateCode,
-  calculateFee
-};
+module.exports = { generateCodeUnique };
