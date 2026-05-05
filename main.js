93425e0ce4e2bb9775ffc6b468d0c724026f299c
Selvandieumeme <infos@fondationbackupspirituel.com>
2026-05-05 01:30:14 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
+const { app, BrowserWindow } = require("electron");
 
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false
+    }
+  });
+
+  win.loadFile("agent.html");
+}
+
+app.whenReady().then(createWindow);
