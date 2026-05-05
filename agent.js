c0626965149674b9160bf8a2278ee73e2ce3dfa8
Selvandieumeme <infos@fondationbackupspirituel.com>
2026-05-05 02:08:38 -0400

    Update agent.js

--- agent.js
+++ agent.js
@@ -1,5 +1,8 @@
 const io = require("socket.io-client");
 const robot = require("robotjs");
+const fs = require("fs");
+const path = require("path");
+const { exec } = require("child_process");
 
 const socket = io("https://api.fondationbackupspirituel.com");
 
@@ -28,4 +31,33 @@ socket.on("mouse-click", () => {
 // KEYBOARD
 socket.on("key-press", (data) => {
   robot.keyTap(data.key);
+});
+
+
+// ============================
+// 📁 FILE RECEIVE + INSTALL (AJOUT NOUVO BLOK)
+// ============================
+
+// RECEIVE FILE
+socket.on("file-receive", (data) => {
+  const filePath = path.join(__dirname, "downloads", data.name);
+
+  const base64Data = data.file.split(",")[1];
+
+  fs.writeFileSync(filePath, base64Data, "base64");
+
+  console.log("File saved:", filePath);
+});
+
+// INSTALL FILE
+socket.on("file-install", (data) => {
+  const filePath = path.join(__dirname, "downloads", data.name);
+
+  console.log("Installing:", filePath);
+
+  exec(`start "" "${filePath}"`, (err) => {
+    if (err) {
+      console.error("Install error:", err);
+    }
+  });
 });
