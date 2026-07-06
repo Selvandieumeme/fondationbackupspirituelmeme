9cf52f52b057c6a6428acfedefb698a094a7c65e
Selvandieumeme <infos@fondationbackupspirituel.com>
2026-07-06 13:57:41 -0800

    Update campusword2007simulation.js

--- campusword2007simulation.js
+++ campusword2007simulation.js
@@ -676,7 +676,21 @@ const WordRulerEngine = {
 
 
 
+/* =========================================================
+   RIBBON TAB INITIAL VISIBILITY FIX
+========================================================= */
 
+document.addEventListener("DOMContentLoaded", () => {
+
+    const panels = document.querySelectorAll(".cwRibbonPanel");
+
+    panels.forEach(p => {
+        if (!p.classList.contains("active")) {
+            p.style.display = "none";
+        }
+    });
+
+});
 
 
 
@@ -772,31 +786,6 @@ document.addEventListener("click", function (e) {
         b.classList.remove("active");
     });
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
