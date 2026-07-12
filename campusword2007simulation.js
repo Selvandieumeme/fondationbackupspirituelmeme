24b7d4689ec01431d47849e5efe86a1d6fde2422
Selvandieumeme <infos@fondationbackupspirituel.com>
2026-07-12 12:41:36 -0800

    Update campusword2007simulation.js

--- campusword2007simulation.js
+++ campusword2007simulation.js
@@ -15345,93 +15345,12 @@ document.addEventListener(
 
 
 
-
-/* =========================================================
-   CAMPUS WORD — INSERT CLIPART BUTTON ENGINE
-   STEP 1
-   BUTTON ACTIVE STATE ONLY
-   NO CLIPART INSERTION YET
-   ISOLATED MODULE
-   NO IMAGE / SHAPE / CARET INTERFERENCE
-========================================================= */
-
-(function(){
-
-
-
-document.addEventListener(
-    "click",
-    function(e){
-
-
-
-        const button =
-            e.target.closest(
-                '[data-action="insert-clipart"]'
-            );
-
-
-
-        if(!button){
-
-            return;
-
-        }
-
-
-
-
-
-        e.preventDefault();
-
-
-        e.stopPropagation();
-
-
-
-
-
-
-
-        button.classList.toggle(
-            "cwInsertClipArtActive"
-        );
-
-
-
-
-
-    },
-    false
-);
-
-
-
-})();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 /* =========================================================
    CAMPUS WORD — INSERT CLIP ART BUTTON ENGINE
    STEP 1
-   BUTTON ACTIVE STATE ONLY
-   MATCH REAL HTML ID
-   NO CLIPART INSERTION YET
-   ISOLATED MODULE
+   ISOLATED BUTTON ACTIVE STATE ONLY
+   TARGET ONLY INSERT CLIPART
+   NO OTHER BUTTON INTERFERENCE
    NO IMAGE / SHAPE / CARET INTERFERENCE
 ========================================================= */
 
@@ -15466,7 +15385,6 @@ document.addEventListener(
 
 
         e.stopPropagation();
-
 
 
 
