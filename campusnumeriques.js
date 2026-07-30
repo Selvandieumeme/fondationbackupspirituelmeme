5cc3a0125ba14ab732596e1a24093d00b9589cf2
Selvandieumeme <infos@fondationbackupspirituel.com>
2026-07-30 14:12:00 -0800

    Update campusnumeriques.js

--- campusnumeriques.js
+++ campusnumeriques.js
@@ -264,6 +264,53 @@ const MicrosoftWordProgressEngine = {
     }
 
 
+
+
+
+// =====================================
+// COMPLETE CHAPTER
+// =====================================
+
+completeChapter(chapterId){
+
+
+    const progress =
+
+    this.get();
+
+
+
+    if(
+
+        !progress.completedChapters.includes(
+
+            chapterId
+
+        )
+
+    ){
+
+        progress.completedChapters.push(
+
+            chapterId
+
+        );
+
+    }
+
+
+
+    this.save(
+
+        progress
+
+    );
+
+
+}
+
+
+
 };
 
 
